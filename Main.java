76548970b0746bca0cbfde14536bf977aca5c2c6
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-11-05 19:02:24 +0000

    .

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class Main {
+  public static void main(String[] args) {
+
+    int usuarioinput = -1, computadorinput; 
+    Scanner sc1 = new Scanner(System.in);
+    Random random = new Random();
+    computadorinput = random.nextInt(1000) + 1;
+      do {
+      System.out.println("Digite um número de 01 a 1000 para participar do sorteio: ");
+    usuarioinput = sc1.nextInt();
+        if (usuarioinput==computadorinput){
+        System.out.println("Parabéns, você ganhou um curso JAVA no Senai Anchieta!!!");
+        }
+        if (usuarioinput < computadorinput) {
+        System.out.println("O número sorteado é maior.");
+        }
+          if  (usuarioinput > computadorinput){
+          System.out.println("O número sorteado é menor.");
+          }
+      } while (usuarioinput != computadorinput);
+      
+    }  
+}
